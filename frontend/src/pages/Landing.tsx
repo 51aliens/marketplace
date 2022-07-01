4748e16cb09ab17f5434cb838889a199106eb8de
Not Thomiz <jeanouina@gmail.com>
2022-07-01 13:20:11 +0200

    fix minting price

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -228,7 +228,7 @@ const Page: FC<{}> = () => {
             params: [],
             methodName: 'safeMint',
             tokenId: VITE_TOKEN_ID,
-            amount: (BigInt(10) * BigInt(10) ** BigInt(18)).toString(), // 10 vite
+            amount: (BigInt(1200) * BigInt(10) ** BigInt(18)).toString(), // 10 vite
           },
     [baycContractAddress, walletAddress]
   );
