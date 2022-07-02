bc1f301d99bf02c5c7268ac8b07905f94cae3684
Not Thomiz <jeanouina@gmail.com>
2022-07-03 00:04:17 +0200

    show address in the website

--- frontend/src/components/shared/Address.tsx
+++ frontend/src/components/shared/Address.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useMemo } from 'react';
-import { useViteProvider } from '@react-vite';
+//import { useViteProvider } from '@react-vite';
 
 import { abbrAddress } from '@utils/string';
 import CopyToClipboard from '@components/shared/CopyToClipboard';
+import { useNavigate } from 'react-router';
 
 const N = 5;
 
@@ -10,15 +11,16 @@ const Address: FC<{ address: string; copy?: boolean }> = ({
   address,
   copy = true,
 }) => {
-  const { baseBlockexplorerUrl } = useViteProvider();
+  //const { baseBlockexplorerUrl } = useViteProvider();
   const formatedAddress = useMemo(() => {
     return abbrAddress(address, N + 5, N);
   }, [address]);
 
-  const blockExplorerLink = useMemo(
+  const blockExplorerLink = `/#/profile/${address}`; /*useMemo(
     () => `${baseBlockexplorerUrl}/address/${address}`,
     [address, baseBlockexplorerUrl]
-  );
+  );*/
+  const navigate = useNavigate();
 
   return (
     <CopyToClipboard {...(copy ? { text: address } : null)}>
@@ -27,6 +29,10 @@ const Address: FC<{ address: string; copy?: boolean }> = ({
         className={'flex items-center text-primary font-bold'}
         target='_blank'
         rel='noreferrer'
+        onClick={(e) => {
+          e.preventDefault();
+          navigate(blockExplorerLink.slice(2));
+        }}
       >
         {formatedAddress}
       </a>
